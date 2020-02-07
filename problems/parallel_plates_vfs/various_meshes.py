92aa3cff3487e22b659e1646fa7e0a001807d2ad
Andrew Franklin <andfranklin3@gmail.com>
2020-02-07 00:21:38 -0600

    Updated various_meshes.py

--- problems/parallel_plates_vfs/various_meshes.py
+++ problems/parallel_plates_vfs/various_meshes.py
@@ -1,22 +1,21 @@
-from extract_view_factors import call_phoenix, analytic_solution
+from utils import make_subdir
+from analytic_functions import c_12_analytic_function as analytic_function
+from extract_view_factors import call_phoenix
 
-quadrature_order = "FOURTH" #["FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH", "SIXTH",
-                    # "SEVENTH", "EIGHTH", "NINTH", "TENTH", "ELEVENTH",
-                     #"TWELFTH", "THIRTEENTH", "FOURTEENTH", "FIFTEENTH",
-                     #"SIXTEENTH", "SEVENTEENTH", "EIGHTTEENTH", "NINTEENTH",
-                     #"TWENTIETH"]
-
+quadrature_order = "FOURTH"
 quadrature_type = "GAUSS"
-cases = ["hh_plates", "ht_plates", "htp_plates", "tpt_plates", "tptp_plates", "tt_plates"]
+# cases = ["hh_plates", "ht_plates", "htp_plates", "tpt_plates", "tptp_plates", "tt_plates"]
+cases = ["pyr_pyr"]
 refinement_level = 0
+analytic_solution = analytic_function(10.0, 10.0, 9.0)
 
-# quadrature_type = "GAUSS"
 for case in cases:
-    results = call_phoenix(case=case, refinement_level=refinement_level, quadrature_type=quadrature_type, quadrature_order=quadrature_order)
-    assert results["lp_front"]["lp_front"] == 0.0
-    assert results["rp_back"]["rp_back"] == 0.0
-    assert abs(results["lp_front"]["rp_back"] - results["rp_back"]["lp_front"]) < 1e-12, (results["lp_front"]["rp_back"], results["rp_back"]["lp_front"])
-    solution = results["lp_front"]["rp_back"]
+    make_subdir(case)
+    results = call_phoenix(case, refinement_level=refinement_level, quadrature_type=quadrature_type, quadrature_order=quadrature_order)
+    assert results["lp_right"]["lp_right"] == 0.0
+    assert results["rp_left"]["rp_left"] == 0.0
+    solution = results["lp_right"]["rp_left"]
+    assert abs(solution - results["rp_left"]["lp_right"]) < 1e-12, (solution, results["rp_left"]["lp_right"])
     error = abs(solution - analytic_solution)
 
     print("case     :", case)
