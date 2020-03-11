13008d01a5c8a2c623849c46dab8129a03aafa22
Andrew Franklin <andfranklin3@gmail.com>
2020-03-10 23:35:37 -0500

    Minor modification to the quadrature refinement script.

--- problems/parallel_plates_vfs/quadrature_refinements.py
+++ problems/parallel_plates_vfs/quadrature_refinements.py
@@ -10,25 +10,26 @@ make_subdir(case)
 
 analytic_solution = analytic_function(10.0, 10.0, 9.0)
 
-# quadrature_orders = ["FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH", "SIXTH",
-#                      "SEVENTH", "EIGHTH", "NINTH", "TENTH", "ELEVENTH",
-#                      "TWELFTH", "THIRTEENTH", "FOURTEENTH", "FIFTEENTH",
-#                      "SIXTEENTH", "SEVENTEENTH", "EIGHTTEENTH"]
 
-quadrature_orders = ["FIRST", "SECOND", "THIRD", "FOURTH"]
+quadrature_orders = ["FIRST", "SECOND", "THIRD", "FOURTH",
+                     "FIFTH", "SIXTH", "SEVENTH", "EIGHTH",
+                     "NINTH", "TENTH", "ELEVENTH", "TWELFTH",
+                     "THIRTEENTH", "FOURTEENTH", "FIFTEENTH",
+                     "SIXTEENTH", "SEVENTEENTH", "EIGHTTEENTH"]
 
 quadrature_numerical_orders = list(range(1, len(quadrature_orders) + 1))
 
 # quadrature_types = ["CLOUGH", "CONICAL", "GAUSS", "GRID", "MONOMIAL",
 #                     "SIMPSON", "TRAP", "GAUSS_LOBATTO"]
 
-# quadrature_types = ["GAUSS"]
-quadrature_types = ["GAUSS", "GRID", "SIMPSON", "TRAP"]
+quadrature_types = ["GAUSS", "GRID", "MONOMIAL", "SIMPSON", "TRAP"]
 
 refinement_level = 0
 for quadrature_type in quadrature_types:
+    print(quadrature_type)
     errors = []
     for quadrature_order in quadrature_orders:
+        print(" ", quadrature_order)
         results = call_phoenix(case, refinement_level=refinement_level, quadrature_type=quadrature_type, quadrature_order=quadrature_order)
         assert results["lp_right"]["lp_right"] == 0.0
         assert results["rp_left"]["rp_left"] == 0.0
@@ -40,9 +41,9 @@ for quadrature_type in quadrature_types:
     plt.plot(quadrature_numerical_orders, errors, label=label, linestyle="-", marker=".")
 
 plt.xlabel("Quadrature Order")
-plt.ylabel("Error")
+plt.ylabel("Absolute Error")
 plt.yscale("log")
 plt.xticks(ticks=quadrature_numerical_orders, labels=[str(order) for order in quadrature_numerical_orders])
 plt.legend()
-plt.savefig('quadrature_refinement.png')
+plt.savefig(f"quadrature_refinement_{case}.png")
 plt.show()
